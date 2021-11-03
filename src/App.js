a908c2b64f482296c074a45caf6855a6cc9505fc
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2021-11-03 12:20:27 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Login />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/login2.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, img, input, Row, Col } from 'react-bootstrap';
+
+export default class login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuario: '',
+      pass: '',
+    };
+  }
+
+  iniciarSesion() {
+    alert(`usuario: ${this.state.usuario} - password: ${this.state.pass}`);
+  }
+
+  render() {
+    return (
+      <Container>
+          <form>
+      <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    </form>
+      </Container>
+    );
+  }
+}
